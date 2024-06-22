313bdd820398810448704d3f5406552c1d7e13d1
maggie <maggie.weng112@gmail.com>
2024-06-22 13:06:01 -0700

    config page layout

---
+++ frontend/app/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <div>
+      <h1 className="font-semibold text-[1.7rem]">Logoo</h1>
+    </div>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,29 @@
 import Link from "next/link";
+import Logo from "./components/Logo";
 
 export default function Home() {
   return (
-    <main>
-      <div>Config page, choose options and then start interview</div>
-      <Link href="/interview-room">Join Now</Link>
+    <main className="main-container">
+      <Logo />
+      <div className="flex flex-row mt-[17vh] w-[100%] items-center">
+        <section className="w-[50%] items-center justify-center">
+          <div className="rounded-[30px] bg-red-300 h-[48vh] aspect-[765/501]"></div>
+        </section>
+        <section className="w-[50%] h-[100%] flex flex-col items-center justify-between">
+          <div>Form here...</div>
+          <JoinButton />
+        </section>
+      </div>
     </main>
+  );
+}
+
+function JoinButton() {
+  return (
+    <button className="font-medium w-[120px] h-[50px] bg-[#6E87ED] text-white rounded-[50px]">
+      <Link className="w-[100%] h-[100%]" href="/interview-room">
+        Join Now
+      </Link>
+    </button>
   );
 }
